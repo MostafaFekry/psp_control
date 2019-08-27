00acca97e9a00a1ce57883aab62d3a27411fc413
systematic-eg <m.fekry@systematiceg.com>
2019-08-27 14:17:35 +0000

    after delete unneed files

--- psp_control/psp_control/doctype/item_to_manufacture_details/item_to_manufacture_details.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2019, Systematic-eg and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('Item To Manufacture Details', {
-	// refresh: function(frm) {
-
-	// }
-});
